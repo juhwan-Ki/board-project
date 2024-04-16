3b7720326e0520a13ca94e8c438d7e04e4a7e720
juhwan <kjhw990218@gmail.com>
2024-04-16 22:15:28 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 액츄레이터
    * 데브툴즈
    * 롬복

---
+++ board-project/src/main/java/com/kevin/boardproject/BoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.boardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectApplication.class, args);
+    }
+
+}
