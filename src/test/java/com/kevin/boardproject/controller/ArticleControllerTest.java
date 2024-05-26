c61d7fcd712827eb9f4b716b666746d516a9e7cd
juhwan <kjhw990218@gmail.com>
2024-05-26 20:33:07 +0900

    #17 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 타입을 확인 했기 때문에 나머지 뷰도 동일할 것으로 예상하여 변경함

--- src/test/java/com/kevin/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/kevin/boardproject/controller/ArticleControllerTest.java
@@ -45,7 +45,7 @@ class ArticleControllerTest {
         // when & then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -62,7 +62,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Disabled("구현 중")
@@ -75,6 +75,6 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
