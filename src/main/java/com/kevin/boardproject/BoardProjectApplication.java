345c5ed44dfab0ec0c7e829c5df3a7f299d1f5aa
juhwan <kjhw990218@gmail.com>
2024-05-26 21:29:40 +0900

    #17 - thymeleaf3 decoupled logic 설정
    ava code로 thymeleaf3 decoupled logic을 사용자 application property로 제어할 수 있도록 추가

--- src/main/java/com/kevin/boardproject/BoardProjectApplication.java
+++ src/main/java/com/kevin/boardproject/BoardProjectApplication.java
@@ -2,7 +2,9 @@ package com.kevin.boardproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class BoardProjectApplication {
 
