4128578e93538d3ebcc369bf7a34daf420e2e5f9
juhwan <kjhw990218@gmail.com>
2024-12-11 23:50:04 +0900

    feat: ArticleCommentService 테스트 추가

---
+++ src/main/java/com/kevin/boardproject/service/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package com.kevin.boardproject.service;
+
+import com.kevin.boardproject.dto.ArticleCommentDto;
+import com.kevin.boardproject.repository.ArticleCommentRepository;
+import com.kevin.boardproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+
+    }
+}
